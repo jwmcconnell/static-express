b6170d3e4a1ce4f25fe953a5c617d7482c3da506
Jackmc <jackmc1122@gmail.com>
2019-07-10 15:55:24 -0700

    Add an endpoint which sends a list of colors from a json file.

--- test/app.test.js
+++ test/app.test.js
@@ -1,12 +1,24 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
+const colors = require('../lib/colors.json');
+
 describe('Static Express Server', () => {
   it('returns an html page', () => {
     return request(app)
       .get('/')
       .then(res => {
         expect(res.text).toEqual(expect.stringContaining('<h1>Colors</h1>'));
+      });
+  });
+});
+
+describe('API endpoints', () => {
+  it('returns an array of colors', () => {
+    return request(app)
+      .get('/api/v1/colors')
+      .then(res => {
+        expect(res.text).toEqual(JSON.stringify(colors));
       });
   });
 });
